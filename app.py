e0d8cb9d6dca5e0ea2d8bfc937ff46530e4cd173
Cindy Wu <cindyofwu@gmail.com>
2014-10-10 11:07:52 -0400

    10/9/14

---
+++ app.py
@@ -0,0 +1,2 @@
+from flask import Flask, request, url_for, redirect, render_template
+
---
+++ something.py
@@ -0,0 +1,6 @@
+#dictionary
+import shelve
+#import flask
+#from flask.ext import shelve
+
+d = shelve.open("data.dat")
